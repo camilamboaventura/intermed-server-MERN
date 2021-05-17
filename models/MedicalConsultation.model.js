ddcf4370bb72e037c67207786acb3b6e6452075b
ialmeida <ialmeida.pb@gmail.com>
2021-05-17 18:06:57 -0300

    done

--- models/MedicalConsultation.model.js
+++ models/MedicalConsultation.model.js
@@ -1,20 +1,10 @@
 const mongoose = require("mongoose");
 
 const MedicalConsultationSchema = mongoose.Schema({
-    pacient_id: {},
-    doctor_id: {},
-    date_of_appointment: {},
-
-
-
-  created_by: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  patient_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  date_of_visit: { type: Date, default: Date.now },
-  allergy: [{ type: String, required: true, trim: true }],
-  chief_complaint: { type: String, required: true, trim: true },
-  history_illness: { type: String, required: true, maxlength: 500, trim: true },
-  medications: [{ type: String, trim: true }],
-  test_results: { type: String },
+    pacient_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    doctor_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    date_of_appointment: { type: Date, required: true },
+    // time_of_appointment: { type: Date, required: true },
 });
 
 module.exports = mongoose.model("MedicalConsultation", MedicalConsultationSchema);
